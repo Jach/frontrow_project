a3410da61e2e72305d77ccd0d439e1b309288b7f
Kevin Secretan <kevinmccloud@gmail.com>
2014-06-04 15:27:46 -0700

    Add new project dependencies for Ring-JSON and Compojure.

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.3.0"]])
+                 [compojure "1.1.6"]
+                 [ring/ring-json "0.3.1"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler frontrow.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
