bd2e1acf27389b36333d69fb51effc54eae31502
Kevin Secretan <kevinmccloud@gmail.com>
2014-06-04 16:47:13 -0700

    Add Jetty server to dependencies. (For learning experience.)

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject frontrow "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :main frontrow.handler
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [ring/ring-json "0.3.1"]]
+                 [ring/ring-json "0.3.1"]
+                 [ring/ring-jetty-adapter "1.3.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler frontrow.handler/app}
   :profiles
