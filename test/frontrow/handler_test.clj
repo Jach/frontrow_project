cbb28b9caf5e6feec5f16367a1d1c358e9776f66
Kevin Secretan <kevinmccloud@gmail.com>
2014-06-04 17:41:57 -0700

    Add test for create-or-conj.

--- test/frontrow/handler_test.clj
+++ test/frontrow/handler_test.clj
@@ -1,6 +1,6 @@
 (ns frontrow.handler-test
   (:require [clojure.test :refer [deftest is testing]]
-            [frontrow.handler :refer [average]]))
+            [frontrow.handler :refer [average create-or-conj]]))
 
 ;;;; Tests for pure functions in handler.clj
 ;;;; lein test :only frontrow.handler-test/test-pure-fns
@@ -9,4 +9,8 @@
   (testing "Test average"
     (is (= (average [0 1 2 3 4]) 2))
     (is (= (float (average [-4 2 0 0])) -0.5))
-    (is (thrown? AssertionError (average [])))))
+    (is (thrown? AssertionError (average []))))
+  (testing "Test create-or-conj"
+    (is (= (create-or-conj {} :a 4) {:a [4]}))
+    (is (= (create-or-conj {:a [4]} :a 5) {:a [4 5]}))
+    (is (= (create-or-conj {:a [4]} :b -4) {:a [4] :b [-4]}))))
